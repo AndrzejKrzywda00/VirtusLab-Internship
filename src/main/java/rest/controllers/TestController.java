2e8d8b4a830a5a9c96be2c8941160433a9d503e6
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-04-14 09:57:29 +0200

    changed the /test -> /

--- src/main/java/rest/controllers/TestController.java
+++ src/main/java/rest/controllers/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-    @RequestMapping("/test")
+    @RequestMapping("/")
     @ResponseBody
     public String testImplementation() {
         return "<h1>Spring was initialized correctly.</h1>";
